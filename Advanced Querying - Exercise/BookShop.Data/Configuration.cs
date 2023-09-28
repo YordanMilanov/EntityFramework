c3fb152bca10032bc9b49135c78fb4a16f7482d1
YordanMilanov <dancho.galinov@gmail.com>
2023-09-28 14:46:04 +0300

    task 2 completed

---
+++ Advanced Querying - Exercise/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=DESKTOP-324OS5E;Database=BookShopSystem;Trusted_Connection=True;Integrated Security=true;Encrypt=false";
+    }
+}
---
+++ Advanced Querying - Exercise/BookShop/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var dbContext = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string ageRestrictionInput = Console.ReadLine();
+            string result = GetBooksByAgeRestriction(dbContext, ageRestrictionInput);
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext dbContext, string command) 
+        {
+            bool hasParsed = Enum.TryParse(typeof(AgeRestriction), command, true, out object? ageRestrictionObject);
+           AgeRestriction ageRestriction;
+
+            if (hasParsed)
+            {
+                ageRestriction = (AgeRestriction)ageRestrictionObject;
+            
+
+                String[] bookTitles = dbContext.Books
+                    .Where(b => b.AgeRestriction == ageRestriction)
+                    .OrderBy(b => b.Title)
+                    .Select(b => b.Title)
+                    .ToArray();
+            return String.Join(Environment.NewLine, bookTitles);
+            }
+            return null;
+        }
+    }
+}
+
+
