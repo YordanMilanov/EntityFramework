0c1212396a6898cb839437377ab652d0ad28d612
YordanMilanov <dancho.galinov@gmail.com>
2023-10-05 10:01:43 +0300

    update

---
+++ LibraryApp-Example/Library/Data/Models/IdentityUserBooks.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace Library.Data.Models
+{
+    public class IdentityUserBooks
+    {
+        public string CollectorId { get; set; } = null!;
+        [ForeignKey(nameof(CollectorId))]
+        public virtual IdentityUser Collector { get; set; } = null!;
+        public int BookId {get; set; }
+
+        [ForeignKey(nameof(BookId))]
+        public virtual Book Book { get; set; } = null!;
+    }
+}
